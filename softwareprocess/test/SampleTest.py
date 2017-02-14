e9661d41115f7a6055cc995f512b7b7db77c88d8
Sipley <breanna.sipley@gmail.com>
2017-02-14 16:18:48 -0600

    Green Light |

---
+++ softwareprocess/test/SampleTest.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+import softwareprocess.Sample as S
+
+class SampleTest(TestCase):
+    pass
+
+#---------------
+#--- Acceptance tests
+#    100 constructor
+#       Desired level of confidence:    Boundary Value Analysis
+#       Input/Output analysis:
+#           input:  n -> integer .GE. 2 and .LT. 30, mandatory, unvalidated
+#           output: instance of Sample
+#       Happy path analysis:
+#           n:  nominal value       4   expected result:    instance of Sample
+#               low bound       2   expected result:    instance of Sample
+#               high bound      29  expected result:    instance of Sample
+#       Sad path analysis:
+#           n:  non-int n       4.2 expected result:    exception
+#           n:  out of bounds n     1   expected result:    exception
+#           n:  out of bounds n     30  expected result:    exception
+#           n is missing        missing n   expected result:    exception
+#
+#   Happy path
+    def test100_010_ShouldConstructInstanceWithNominalValue(self):
+        self.assertIsInstance(S.Sample(4),S.Sample)
