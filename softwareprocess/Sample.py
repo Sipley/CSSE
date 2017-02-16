7fbfdb34fdb256635d2bda31b2aea58c2d4594d0
Sipley <breanna.sipley@gmail.com>
2017-02-16 15:53:14 -0600

    Red Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -1,7 +1,7 @@
 class Sample():
     def __init__(self,n=None):
         if(n == None):
-            raise ValueError("Sample.__init__():  invalid n")
+            raise ValueError('Sample.__init__:  invalid n')
         self.n = n
 
     def getN(self):
