789ba4738470f36a435ee8284f0f1c7986523063
Sipley <breanna.sipley@gmail.com>
2017-02-16 15:53:51 -0600

    Red Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -2,6 +2,8 @@ class Sample():
     def __init__(self,n=None):
         if(n == None):
             raise ValueError('Sample.__init__:  invalid n')
+        #if((n<2) or (n>29)):
+        #    raise ValueError('Sample.__init__:  invalid n')
         self.n = n
 
     def getN(self):
--- softwareprocess/test/SampleTest.py
+++ softwareprocess/test/SampleTest.py
@@ -39,20 +39,69 @@ class SampleTest(TestCase):
         self.assertIsInstance(myS,S.Sample)
         self.assertEquals(myS.getN(), 29)
 
-    def test100_910_ShouldRaiseExceptionOnMissingN(self):
-        #decide what string going to raise
-        #'invalid n'
-        expectedString = "Sample.__init__:  invalid n"
-        #want to raise an exception with name of class & name of method that raises exception
-        with self.assertRaises(ValueError) as context:
-        #within next statements, should raise value error.  If doesn't, bad
-        #if does, good
-        #and if does, should assign to variable called context
-        #now instantiate
-            myS = S.Sample()
-        self.assertEqual(expectedString, context.exception.args[0][0:len(expectedString)])
-        #if doesn't, fail right here & gimme red light
-        #check to make sure that argument problem is string
-        #red light
-        #good, is red light
-        #now, let's write production code to fix this
+    if __name__ == '__main__':
+        if __name__ == '__main__':
+            def test100_910_ShouldRaiseExceptionOnMissingN(self):
+                #decide what string going to raise
+                #'invalid n'
+                expectedString = "Sample.__init__:  invalid n"
+                #want to raise an exception with name of class & name of method that raises exception
+                with self.assertRaises(ValueError) as context:
+                #within next statements, should raise value error.  If doesn't, bad
+                #if does, good
+                #and if does, should assign to variable called context
+                #now instantiate
+                    myS = S.Sample()
+                self.assertEqual(expectedString, context.exception.args[0][0:len(expectedString)])
+                #if doesn't, fail right here & gimme red light
+                #check to make sure that argument problem is string
+                #red light
+                #good, is red light
+                #now, let's write production code to fix this
+
+            '''
+            #boundary level analysis.  So want to have tests that test boundaries
+            #look at nominal value.  Then lower boundary & upper boundary
+            #HAPPY PATH == nominal operation of software.  Normal stuff w/o delving into any problem areas
+                #only thing we have going in is n
+                #start with nominal value (e.g., 4)
+                #expected result: get instance of sample
+                #also want to expect boundary (e.g., 2 & 29)
+                #but wait, what are we trying to accomplish there
+            #SAD PATH == something that could arise
+                #what happens if n is not an integer?
+                #Need to check n
+                #Is it there?
+                #Is it an integer?
+                #Also boundaries
+            #when being: want failing test case
+                #want to fail first time around, but we want it to provide us some information
+                #construct test case that will take us a little further
+                #ask what isn't there, then it'll give us some info, so we can add production code
+                #ran okay, but we expected red light, but got green light
+                #couldn't differ btwn n of 2 versus 4
+                #add in a method to test whether it works
+            #we're gonna stay away from void functions: functions that return nothing
+                #we want to return something
+                #will give us more info than if we got nothing back
+            #added my.S, S.sample, but didn't tell us anything
+                #so we added getN, & that was cool
+            #then we started on Sad path
+                #what happened if we leave n out?
+                #we said, if this happens, then we expect exception to be raised
+                #if raised, assign to variable called context, then peel off string to see whether it's actually what we want
+            #now, let's work on bounds
+            '''
+
+        #want to exercise BDD.  what behavior do we want to drive this?
+            #we want to raise an exception should n be out of bounds
+
+            def test100_920_ShouldRaiseExceptionOnLowN(self):
+                expectedString = "Sample.__init__: invalid n"
+                with self.assertRaises(ValueError) as context:
+                    myS = S.Sample()
+                self.assertEqual(expectedString, context.exception.args[0][0:leng(expectedString)])
+
+
+
+
