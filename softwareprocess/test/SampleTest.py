c26b6cc3abafb3fa8fe7782d8215253237cb4572
Sipley <breanna.sipley@gmail.com>
2017-02-16 16:00:10 -0600

    Green Light |

--- softwareprocess/test/SampleTest.py
+++ softwareprocess/test/SampleTest.py
@@ -40,19 +40,18 @@ class SampleTest(TestCase):
         self.assertEquals(myS.getN(), 29)
 
     if __name__ == '__main__':
-        if __name__ == '__main__':
-            def test100_910_ShouldRaiseExceptionOnMissingN(self):
+        def test100_910_ShouldRaiseExceptionOnMissingN(self):
                 #decide what string going to raise
                 #'invalid n'
-                expectedString = "Sample.__init__:  invalid n"
+            expectedString = "Sample.__init__:  invalid n"
                 #want to raise an exception with name of class & name of method that raises exception
-                with self.assertRaises(ValueError) as context:
+            with self.assertRaises(ValueError) as context:
                 #within next statements, should raise value error.  If doesn't, bad
                 #if does, good
                 #and if does, should assign to variable called context
                 #now instantiate
-                    myS = S.Sample()
-                self.assertEqual(expectedString, context.exception.args[0][0:len(expectedString)])
+                 myS = S.Sample()
+            self.assertEqual(expectedString, context.exception.args[0][0:len(expectedString)])
                 #if doesn't, fail right here & gimme red light
                 #check to make sure that argument problem is string
                 #red light
@@ -102,7 +101,7 @@ class SampleTest(TestCase):
                     myS = S.Sample(1)
                 self.assertEqual(expectedString, context.exception.args[0][0:len(expectedString)])
 
-            def test100_920_ShouldRaiseExceptionOnLowN(self):
+            def test100_930_ShouldRaiseExceptionOnNonIntN(self):
                 expectedString = "Sample.__init__: invalid n"
                 with self.assertRaises(ValueError) as context:
                     myS = S.Sample(1)
