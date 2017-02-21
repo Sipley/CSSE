4d5f345cf2f792387860ee650ec3bd49217720a0
Sipley <breanna.sipley@gmail.com>
2017-02-21 03:02:27 -0600

    Red Light |

--- test/SampleTest.py
+++ test/SampleTest.py
@@ -270,6 +270,6 @@ class SampleTest(unittest.TestCase):
         mySample = SM.Sample(self.nominalN)
         self.assertEquals(mySample.simpsonBuild(0, 2), 2)
 
-    def test500_020_ShouldAddCalcAllOtherEvenTerms:
+    def test500_020_ShouldAddCalcAllOtherEvenTerms(self):
         mySample = SM.Sample(self.nominalN)
         self.assertEquals(mySample.simpsonBuild(0, 1), 1)
