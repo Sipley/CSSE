bcf1a18019bf0e280a954ad9331ce2578d344c46
Sipley <breanna.sipley@gmail.com>
2017-02-20 23:42:11 -0600

    Green Light |

--- test/SampleTest.py
+++ test/SampleTest.py
@@ -251,4 +251,4 @@ class SampleTest(unittest.TestCase):
 
     def test500_010_ShouldCalculateIntegralOfF(self):
         mySample = SM.Sample(self.nominalN)
-        self.assertEquals(mySample.integrate(0, 1, 5, f), 0.5)
+        self.assertEquals(mySample.integrate(0, 1, 5), 0.5)
