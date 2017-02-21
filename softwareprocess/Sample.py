2b77c2642bfcd50b2589c269f223dbbff44f40a0
Sipley <breanna.sipley@gmail.com>
2017-02-21 15:58:22 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -97,7 +97,7 @@ class Sample(object):
         slices = 4
         w = (highBound - lowBound) / slices
         simpsonNew = f(lowBound, n)
-        simpsonNew = += 4.0 * f(lowBound + 1.0 * w, n) + \
+        simpsonNew += 4.0 * f(lowBound + 1.0 * w, n) + \
                         2.0 * f(lowBound + 2.0 * w, n) + \
                         4.0 * f(lowBound + 3.0 * w, n)
         simpsonNew += f(highBound, n)
