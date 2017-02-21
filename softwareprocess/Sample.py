34649ea17e97dfa5d0743dc053552ffe8971c5f1
Sipley <breanna.sipley@gmail.com>
2017-02-20 23:49:21 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -67,7 +67,7 @@ class Sample(object):
     def f(self, u, n):
         return u
 
-    def integrate(lowerBound, upperBound, n):
+    def integrate(lowerBound, upperBound, n, f()):
         pass
 
 
