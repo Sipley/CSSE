59dfb63bbfef775f7bfa29c0837b0aba71b4a753
Sipley <breanna.sipley@gmail.com>
2017-03-22 13:18:38 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -31,7 +31,7 @@ def dispatch(values=None):
                 values['error'] = 'observation is invalid'
         else:
             values['error'] = 'observation is invalid'
-        if not(isinstance(values['height'], int))):
+        if not(isinstance(values['height'], int)):
             values['error'] = 'observation is invalid'
         return values    #<-------------- replace this with your implementation
     elif(values['op'] == 'predict'):
