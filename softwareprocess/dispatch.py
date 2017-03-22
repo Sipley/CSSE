68d05bb36ef719c0c0ca070678b24c35bdc287fb
Sipley <breanna.sipley@gmail.com>
2017-03-22 11:38:16 -0500

    Refactor |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -33,7 +33,7 @@ def dispatch(values=None):
         degrees = observation.split('d')[1]
         if(not(isinstance(altitude, int))):
             values['error'] = 'observation is invalid'
-        if(len(degrees)=0)):
+        if(len(degrees) == 0)):
             values['error'] = 'observation is invalid'
         return values    #<-------------- replace this with your implementation
     elif(values['op'] == 'predict'):
