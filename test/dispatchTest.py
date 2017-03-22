83f7dd3153e639df53ed97186c5b517d9c3b7a0e
Sipley <breanna.sipley@gmail.com>
2017-03-22 13:54:10 -0500

    Green Light |

--- test/dispatchTest.py
+++ test/dispatchTest.py
@@ -166,5 +166,5 @@ class dispatchTest(unittest.TestCase):
     def test900_190_shouldReturnErrorInvalidTemp(self):
         sighting = {'op':'adjust','observation':'0d0.1','temperature':'-21'}
         result = dispatch.dispatch(sighting)
-        expectedResult = {'error':'temperature is invalid','op':'adjust','observation':'0d0.1','height':'string'}
+        expectedResult = {'error':'temperature is invalid','op':'adjust','observation':'0d0.1','temperature':'-21'}
         self.assertDictEqual(result, expectedResult)
