df41b5048302fc08265cd8a921e9356f6a5557ec
Sipley <breanna.sipley@gmail.com>
2017-03-22 18:31:43 -0500

    Green Light |

--- test/dispatchTest.py
+++ test/dispatchTest.py
@@ -262,5 +262,5 @@ class dispatchTest(unittest.TestCase):
     def test900_350_shouldReturnErrorDegreeNotFloat(self):
         sighting = {'op':adjust','obervation':'0d1'}
         result = dispatch.dispatch(sighting)
-        expectedResult = {'error':'height is invalid','op':'adjust','observation':'0d0.1','height':'-1'}
+        expectedResult = {'error':'obervation is invalid','op':'0d1}
         self.assertDictEqual(result, expectedResult)
