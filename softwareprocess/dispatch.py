8a3cdc9edf36811890795114e44ce9abce9eeb23
Sipley <breanna.sipley@gmail.com>
2017-03-22 22:22:51 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -49,7 +49,7 @@ def dispatch(values=None):
                     values['error'] = 'pressure is invalid'
                     return values
             if ('horizon' in values):
-                if not(values['horizon']) == ('artificial' or 'Artificial' or 'natural' or 'Natural')):
+                if not(values['horizon']) == ('artificial' or 'Artificial' or 'natural' or 'Natural'):
                 values['error'] = 'horizon is invalid'
                 return values
 
