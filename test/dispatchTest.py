2dfd53496b56196348cf9b3c5beb8fa5492aa051
Sipley <breanna.sipley@gmail.com>
2017-03-22 22:11:04 -0500

    Green Light |

--- test/dispatchTest.py
+++ test/dispatchTest.py
@@ -51,17 +51,30 @@ class dispatchTest(unittest.TestCase):
     #    expectedResult = {'altitude':'41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
     #    self.assertDictEqual(result, expectedResult)
 
-    def test100_040_shouldReturnAltWithParm(self):
-        sighting = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        result = dispatch.dispatch(sighting)
-        expectedResult = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        self.assertDictEqual(result, expectedResult)
+    #def test100_040_shouldReturnAltWithParm(self):
+    #    sighting = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+    #    result = dispatch.dispatch(sighting)
+    #    expectedResult = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+    #    self.assertDictEqual(result, expectedResult)
 
-    def test100_050_shouldReturnCorrectAlt(self):
-        sighting = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+    #def test100_050_shouldReturnCorrectAlt(self):
+    #    sighting = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+    #    result = dispatch.dispatch(sighting)
+    #    expectedResult = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+    #    self.assertDictEqual(result, expectedResult)
+
+    def test100_060_shouldReturnCorrectAltFromCorrection(self):
+        sighting = {'op':'adjust','observation':'10d0.0','height':'6','horizon':'artificial','pressure':'1010','temperature':'72'}
         result = dispatch.dispatch(sighting)
         expectedResult =
         self.assertDictEqual(result, expectedResult)
+
+    def test100_060_lastHappyTestttttt(self):
+        sighting = {'op':'adjust','observation':'10d0.0','height':'6','horizon':'artificial','pressure':'1010','temperature':'72'}
+        result = dispatch.dispatch(sighting)
+        expectedResult =
+        self.assertDictEqual(result, expectedResult)
+
 # SAD PATH
 
     def test900_010_shouldReturnErrorNoOp(self):
