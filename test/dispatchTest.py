cc7b432c3666e44adc3b509edc99687c46a03cc3
Sipley <breanna.sipley@gmail.com>
2017-03-22 22:07:43 -0500

    Red Light |

--- test/dispatchTest.py
+++ test/dispatchTest.py
@@ -57,6 +57,11 @@ class dispatchTest(unittest.TestCase):
         expectedResult = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         self.assertDictEqual(result, expectedResult)
 
+    def test100_050_shouldReturnCorrectAlt(self):
+        sighting = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        result = dispatch.dispatch(sighting)
+        expectedResult =
+        self.assertDictEqual(result, expectedResult)
 # SAD PATH
 
     def test900_010_shouldReturnErrorNoOp(self):
