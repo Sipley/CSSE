e0142ddae773642d7d55d60673ce4bffac9f7c7a
Sipley <breanna.sipley@gmail.com>
2017-03-22 22:22:26 -0500

    Green Light |

--- test/dispatchTest.py
+++ test/dispatchTest.py
@@ -57,17 +57,17 @@ class dispatchTest(unittest.TestCase):
     #    expectedResult = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
     #    self.assertDictEqual(result, expectedResult)
 
-    #def test100_050_shouldReturnCorrectAlt(self):
-    #    sighting = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #    result = dispatch.dispatch(sighting)
-    #    expectedResult = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-    #    self.assertDictEqual(result, expectedResult)
-
-    def test100_060_shouldReturnCorrectAltFromCorrection(self):
-        sighting = {'op':'adjust','observation':'10d0.0','height':'6','horizon':'artificial','pressure':'1010','temperature':'72'}
+    def test100_050_shouldReturnCorrectAlt(self):
+        sighting = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         result = dispatch.dispatch(sighting)
-        expectedResult = {'altitude':'9d54.7','op':'adjust','observation':'10d0.0','height':'6','horizon':'artificial','pressure':'1010','temperature':'72'}
+        expectedResult = {'altitude':'45d12.0', 'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         self.assertDictEqual(result, expectedResult)
+
+    #def test100_060_shouldReturnCorrectAltFromCorrection(self):
+    #    sighting = {'op':'adjust','observation':'10d0.0','height':'6','horizon':'artificial','pressure':'1010','temperature':'72'}
+    #    result = dispatch.dispatch(sighting)
+    #    expectedResult = {'altitude':'9d54.7','op':'adjust','observation':'10d0.0','height':'6','horizon':'artificial','pressure':'1010','temperature':'72'}
+    #    self.assertDictEqual(result, expectedResult)
 
     #def test100_060_lastHappyTestttttt(self):
     #    sighting = {'op':'adjust','observation':'45d15.2','height':'6','horizon':'natural','pressure':'1010','temperature':'71'}
