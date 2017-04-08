c4c6819185d78e8416c89a1b1e11d4762655dc31
Sipley <breanna.sipley@gmail.com>
2017-04-08 03:51:12 -0500

    Red Light |

--- test/dispatchPredictTest.py
+++ test/dispatchPredictTest.py
@@ -1,10 +1,39 @@
 import unittest
 
+import softwareprocess.prod.dispatch as DP
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class dispatchPredict(unittest.TestCase):
 
+##############ACCEPTANCE TESTS##############
+#body should be string
+#body should match "stars"
+#body should be case-insensitive
+#MANDATORY
+#
+#date string
+#date format yyyy-mm-dd
+#2001 >= yyyy >= 2100
+#01 <= mm <= 12
+#00 <= dd <= 31
+#optional
+#defaults to 2001-01-01
+#
+#time string
+#time format hh:mm:ss
+#00 <= hh <= 24
+#00 <= mm <= 59
+#00 <= ss <= 59
+#time <= 24:00:00
+#time <= 23:59:00
+#time <= 23:59:59
+#
+#if lat &/or long exist, return error
+
+####sad path
+
+    def test900_010_shouldReturnErrorInvalidStar(self):
+        values=
+        self.assertTrue(DP.dispatch(values).has_key("error"), True)
 
 if __name__ == '__main__':
     unittest.main()
