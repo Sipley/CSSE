30634631106fc43618e51eedaeb91f66ff971123
Sipley <breanna.sipley@gmail.com>
2017-04-08 04:20:54 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -7,7 +7,7 @@ class Stars(objects):
         self.SiderealHourAngle = SiderealHourAngle
         self.Declination = Declination
 
-    stars = {Alpheratz, Ankaa, Schedar, Diphda, Achernar, Hamal, Polaris, Akamar, Menkar, Mirfak, Aldebaran, Rigel, Capella, Bellatrix, Elnath,Alnilam,Betelgeuse,Canopus,Sirius,Adara,Procyon,Pollux,Avior,Suhail,Miaplacidus,Alphard,Regulus,Dubhe,Denebola,Gienah,Acrux,Gacrux,Alioth,Spica,Alcaid,Hadar,Menkent,Arcturus,Rigil Kent.,Zubenelg.,Kochab,Alphecca,Antares,Atria,Sabik,Shaula,Rasalhague,Etamin,Kaus Aust.,Vega,Nunki,Altair,Peacock,Deneb,Enif,Alnair,Fomalhaut,Scheat,Markab}
+    stars = {'Alpheratz', 'Ankaa', 'Schedar', 'Diphda', 'Achernar', 'Hamal', 'Polaris', 'Akamar', 'Menkar', 'Mirfak', 'Aldebaran', 'Rigel', 'Capella', 'Bellatrix', 'Elnath','Alnilam','Betelgeuse','Canopus','Sirius','Adara','Procyon','Pollux','Avior','Suhail','Miaplacidus','Alphard','Regulus','Dubhe','Denebola','Gienah','Acrux','Gacrux','Alioth','Spica','Alcaid','Hadar','Menkent','Arcturus','Rigil Kent.','Zubenelg.','Kochab','Alphecca','Antares','Atria','Sabik','Shaula','Rasalhague','Etamin','Kaus Aust.','Vega','Nunki','Altair','Peacock','Deneb','Enif','Alnair','Fomalhaut','Scheat','Markab'}
 
 def dispatch(values=None):
 
