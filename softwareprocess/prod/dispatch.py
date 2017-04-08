a281366485ae2a2486b9263bbbb2001ef888574f
Sipley <breanna.sipley@gmail.com>
2017-04-08 05:28:11 -0500

    Red Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -8,8 +8,8 @@ class Stars():
         self.Declination = Declination
 
     stars = {'Alpheratz', 'Ankaa', 'Schedar', 'Diphda', 'Achernar', 'Hamal', 'Polaris', 'Akamar', 'Menkar', 'Mirfak', 'Aldebaran', 'Rigel', 'Capella', 'Bellatrix', 'Elnath','Alnilam','Betelgeuse','Canopus','Sirius','Adara','Procyon','Pollux','Avior','Suhail','Miaplacidus','Alphard','Regulus','Dubhe','Denebola','Gienah','Acrux','Gacrux','Alioth','Spica','Alcaid','Hadar','Menkent','Arcturus','Rigil Kent.','Zubenelg.','Kochab','Alphecca','Antares','Atria','Sabik','Shaula','Rasalhague','Etamin','Kaus Aust.','Vega','Nunki','Altair','Peacock','Deneb','Enif','Alnair','Fomalhaut','Scheat','Markab'}
-    index = stars.index(body)
-    declination= {}
+    declination= {'29d10.9','-42d13.4','56d37.7','-17d54.1','-57d09.7','23d32.3','89d20.1','-40d14.8','4d09.0','49d55.1','16d32.3','-8d11.3','46d00.7','6d21.6','28d37.1','-1d11.8','7d24.3','-52d42.5','-16d44.3','-28d59.9','5d10.9','27d59.0','-59d33.7','-43d29.8','-69d46.9','-8d43.8','11d53.2','61d39.5','14d28.9','-17d37.7','-63d10.9','-57d11.9','55d52.1','-11d14.5','49d13.8','-60d26.6','-36d26.6','19d06.2','-60d53.6','-16d06.3','74d05.2','26d39.7','-26d27.8','-69d03.0','-15d44.4','-37d06.6','12d33.1','51d29.3','-34d22.4','38d48.1','-26d16.4','8d54.8','-56d41.0','45d20.5','9d57.0','-46d53.1','-29d32.3','28d10.3','15d17.6'}
+
    # def findAngRelAries(body)
 
 def dispatch(values=None):
@@ -120,6 +120,10 @@ def dispatch(values=None):
         if ('lat' or 'long') in values:
             values['error'] = 'lat and/or long already in dict'
             return values
+        index = stars.index(body)
+        dec = stars.declination[index]
+        latitude = dec
+        values['lat'] = latitude
         return values
     elif(values['op'] == 'correct'):
         return values    #This calculation is stubbed out
