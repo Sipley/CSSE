b7542087b854fb15e03eea45d24150998aa97143
Sipley <breanna.sipley@gmail.com>
2017-04-08 19:37:26 -0500

    Green Light |

--- test/dispatchPredictTest.py
+++ test/dispatchPredictTest.py
@@ -38,7 +38,7 @@ class dispatchPredict(unittest.TestCase):
     def test100_010_shouldReturnAngleConvertedFromDeg(self):
         angle = '100d42.6'
         expectedResult = '100.71'
-        self.asssertIsEqual(DP.convertAngleFromDeg(angle),expectedResult)
+        self.assertEqual(DP.convertAngleFromDeg(angle),expectedResult)
 
 ####sad path
 
