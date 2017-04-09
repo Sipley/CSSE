a3a23014eb8a6024771fd8a1e9174b46fdb15ee4
Sipley <breanna.sipley@gmail.com>
2017-04-08 20:29:02 -0500

    Green Light |

--- test/Sandbox.py
+++ test/Sandbox.py
@@ -1,10 +1,9 @@
-values = {'date':'2001-01-01'}
 
-class Paramaters():
-    def __init__(self, values):
-        pass
 
-    date = values['date']
-    print date
+stars = ['Alpheratz', 'Ankaa', 'Schedar', 'Diphda', 'Achernar', 'Hamal', 'Polaris', 'Akamar', 'Menkar', 'Mirfak', 'Aldebaran', 'Rigel', 'Capella', 'Bellatrix', 'Elnath','Alnilam','Betelgeuse','Canopus','Sirius','Adara','Procyon','Pollux','Avior','Suhail','Miaplacidus','Alphard','Regulus','Dubhe','Denebola','Gienah','Acrux','Gacrux','Alioth','Spica','Alcaid','Hadar','Menkent','Arcturus','Rigil Kent.','Zubenelg.','Kochab','Alphecca','Antares','Atria','Sabik','Shaula','Rasalhague','Etamin','Kaus Aust.','Vega','Nunki','Altair','Peacock','Deneb','Enif','Alnair','Fomalhaut','Scheat','Markab']
 
-print Paramaters.date
+StArS = [star.lower() for star in stars]
+print StArS
+
+print stars
+print 'star'.lower()
