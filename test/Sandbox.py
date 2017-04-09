f9c4df524e981b6f79425170b1e62db2a536147b
Sipley <breanna.sipley@gmail.com>
2017-04-08 21:13:24 -0500

    Green Light |

--- test/Sandbox.py
+++ test/Sandbox.py
@@ -2,8 +2,4 @@
 
 stars = ['Alpheratz', 'Ankaa', 'Schedar', 'Diphda', 'Achernar', 'Hamal', 'Polaris', 'Akamar', 'Menkar', 'Mirfak', 'Aldebaran', 'Rigel', 'Capella', 'Bellatrix', 'Elnath','Alnilam','Betelgeuse','Canopus','Sirius','Adara','Procyon','Pollux','Avior','Suhail','Miaplacidus','Alphard','Regulus','Dubhe','Denebola','Gienah','Acrux','Gacrux','Alioth','Spica','Alcaid','Hadar','Menkent','Arcturus','Rigil Kent.','Zubenelg.','Kochab','Alphecca','Antares','Atria','Sabik','Shaula','Rasalhague','Etamin','Kaus Aust.','Vega','Nunki','Altair','Peacock','Deneb','Enif','Alnair','Fomalhaut','Scheat','Markab']
 
-starsInsensitive = [star.lower() for star in stars]
-print StArS
 
-print stars
-print 'star'.lower()
--- test/dispatchCustomer.py
+++ test/dispatchCustomer.py
@@ -1,33 +1,7 @@
 
 
-import softwareprocess.dispatch as dispatch
-sighting = sighting = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-result = dispatch.dispatch(sighting)
+import softwareprocess.prod.dispatch as DP
+values = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+
+result = DP.dispatch(values)
 print result
-
-'''
-import re
-observation = '10d1.1'
-
-if re.match('\d+d\d+\.\d', observation):
-    print 'yes'
-else:
-    print 'no'
-'''
-
-'''
-test = '12d'
-left = test.split('d')[0]
-right = test.split('d')[1]
-print left
-print len(right)
-
-'''
-'''
-print '%dd%.1f' %(12, 12.5)
-'''
-'''
-%d integer
-%1.3f (float d.ddd)
-%s string
-'''
