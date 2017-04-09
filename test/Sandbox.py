0ddd4d56d17806a4f36c1a572dbb5f21b9d65563
Sipley <breanna.sipley@gmail.com>
2017-04-08 20:29:40 -0500

    Green Light |

--- test/Sandbox.py
+++ test/Sandbox.py
@@ -2,7 +2,7 @@
 
 stars = ['Alpheratz', 'Ankaa', 'Schedar', 'Diphda', 'Achernar', 'Hamal', 'Polaris', 'Akamar', 'Menkar', 'Mirfak', 'Aldebaran', 'Rigel', 'Capella', 'Bellatrix', 'Elnath','Alnilam','Betelgeuse','Canopus','Sirius','Adara','Procyon','Pollux','Avior','Suhail','Miaplacidus','Alphard','Regulus','Dubhe','Denebola','Gienah','Acrux','Gacrux','Alioth','Spica','Alcaid','Hadar','Menkent','Arcturus','Rigil Kent.','Zubenelg.','Kochab','Alphecca','Antares','Atria','Sabik','Shaula','Rasalhague','Etamin','Kaus Aust.','Vega','Nunki','Altair','Peacock','Deneb','Enif','Alnair','Fomalhaut','Scheat','Markab']
 
-StArS = [star.lower() for star in stars]
+starsInsensitive = [star.lower() for star in stars]
 print StArS
 
 print stars
