0fee2995709d63e200996b3c97c91561f3e049f9
Sipley <breanna.sipley@gmail.com>
2017-04-27 02:20:45 -0500

    Refactor |

--- test/dispatchCorrectTest.py
+++ test/dispatchCorrectTest.py
@@ -16,12 +16,12 @@ class MyTestCase(unittest.TestCase):
         values={'op':'correct','lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct','lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'invalid lat'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
-
+    '''
     def test900_040_shouldReturnErrorInvAssumedLat(self):
         values={'op':'correct','lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-153d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct','lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-153d38.4', 'assumedLong':'74d35.3', 'error':'invalid assumedLat'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
-
+    '''
     def test900_050_shouldReturnErrorNoLong(self):
         values={'op':'correct', 'lat': '16d32.3', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct', 'lat': '16d32.3', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'mandatory information is missing'}
