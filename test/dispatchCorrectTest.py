1f800229ce2156948cf0ba609dfebb9fc1b07f93
Sipley <breanna.sipley@gmail.com>
2017-04-26 23:59:31 -0500

    Red Light |

--- test/dispatchCorrectTest.py
+++ test/dispatchCorrectTest.py
@@ -2,35 +2,45 @@ import unittest
 import softwareprocess.prod.dispatch as DP
 
 class MyTestCase(unittest.TestCase):
-    def test100_010_shouldReturnErrorMissingInfo(self):
+    def test900_010_shouldReturnErrorMissingInfo(self):
         values={'op':'correct'}
         expectedResult={'op':'correct','error':'mandatory information is missing'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
 
-    def test100_020_shouldReturnErrorMissingInfo(self):
+    def test900_020_shouldReturnErrorMissingInfo(self):
         values={'op':'correct', 'long':'95d41.6', 'altitude':'13d42.4', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct', 'long':'95d41.6', 'altitude':'13d42.4', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'mandatory information is missing'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
 
-    def test100_030_shouldReturnErrorInvLat(self):
+    def test900_030_shouldReturnErrorInvLat(self):
         values={'op':'correct','lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct','lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'invalid lat'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
 
-    def test100_040_shouldReturnErrorInvAssumedLat(self):
+    def test900_040_shouldReturnErrorInvAssumedLat(self):
         values={'op':'correct','lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-153d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct','lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-153d38.4', 'assumedLong':'74d35.3', 'error':'invalid assumedLat'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
 
-    def test100_050_shouldReturnErrorNoLong(self):
+    def test900_050_shouldReturnErrorNoLong(self):
         values={'op':'correct', 'lat': '16d32.3', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct', 'lat': '16d32.3', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'mandatory information is missing'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
 
-    def test100_060_shouldReturnErrorNoAlt(self):
+    def test900_060_shouldReturnErrorNoAlt(self):
         values={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'mandatory information is missing'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
-        
+
+    def test900_070_shouldReturnErrorNoAssumedLat(self):
+        values={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'altitude':'13d42.3','assumedLong':'74d35.3'}
+        expectedResult={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'altitude':'13d42.3','assumedLong':'74d35.3', 'error':'mandatory information is missing'}
+        self.assertDictEqual(DP.dispatch(values),expectedResult)
+
+    def test900_080_shouldReturnErrorNoAssumedLong(self):
+        values={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4'}
+        expectedResult=
+        self.assertDictEqual(DP.dispact(values),expectedResult)
+
 if __name__ == '__main__':
     unittest.main()
