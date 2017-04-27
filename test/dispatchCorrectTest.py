bc3da54c0be5c14c3c9c2379e6563662ed5e70e3
Sipley <breanna.sipley@gmail.com>
2017-04-26 23:20:11 -0500

    Green Light |

--- test/dispatchCorrectTest.py
+++ test/dispatchCorrectTest.py
@@ -12,5 +12,8 @@ class MyTestCase(unittest.TestCase):
         expectedResult={'op':'correct', 'long':'95d41.6', 'altitude':'13d42.4', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'error':'mandatory information is missing'}
         self.assertDictEqual(DP.dispatch(values),expectedResult)
 
+    def test100_030_shouldReturnError
+        values={'op':'correct','lat':'16.0d32.3', 'long':'95.41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
+
 if __name__ == '__main__':
     unittest.main()
