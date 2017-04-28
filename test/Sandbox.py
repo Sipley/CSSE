1e58bc4be40e063e1e2bf25e80190a3e1458e07f
Sipley <breanna.sipley@gmail.com>
2017-04-28 13:51:39 -0500

    Green Light |

--- test/Sandbox.py
+++ test/Sandbox.py
@@ -1,10 +1,31 @@
-values = {'op':'correct'}
+#values = {'op':'correct'}
+
+
+#class Values(object):
+#    def __init__(self, op=None):
+#        self.op = op
+
+
+#result = Values(**values)
+#print result.op
+
+values={'op':'correct','lat':'16d32.3', 'long':'95.41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
 
 
 class Values(object):
-    def __init__(self, op=None):
+    def __init__(self, op=None, lat=None, long=None, altitude=None, assumedLat=None, assumedLong=None):
         self.op = op
+        self.lat = lat
+        self.long = long
+        self.altitude = altitude
+        self.assumedLat = assumedLat
+        self.assumedLong = assumedLong
+
+    result = Values(**values)
+    return result.lat
+
+def checkDict(values):
+    result = Values(**values)
+    return result.lat
 
 
-result = Values(**values)
-print result.op
