a83797dc4853a439be93b623bc60a217d101ccbf
Sipley <breanna.sipley@gmail.com>
2017-04-28 15:25:00 -0500

    Green Light |

--- softwareprocess/prod/dispatch.py
+++ softwareprocess/prod/dispatch.py
@@ -376,6 +376,6 @@ def calcCorrectedAlt(values):
     correctedAlt = math.asin(calcIntDist(values))
     return correctedAlt
 
-#def calcCorrectDistance(values):
-#    correctDistance = Values(**values).altitudeDeg - Values(**values).calcCorrectedAlt(values)
-#    return correctDistance
+def calcCorrectDistance(values):
+    correctDistance = Values(**values).altitudeDeg - Values(**values).calcCorrectedAlt(values)
+    return correctDistance
