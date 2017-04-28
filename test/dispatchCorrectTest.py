ae2de9fed97193b35f26b27e1d07d967bea7aaf1
Sipley <breanna.sipley@gmail.com>
2017-04-28 15:13:28 -0500

    Green Light |

--- test/dispatchCorrectTest.py
+++ test/dispatchCorrectTest.py
@@ -17,7 +17,7 @@ class MyTestCase(unittest.TestCase):
 
     def test100_020_shouldReturnIntDistance(self):
         values={'op':'correct','lat':'89d20.1','long':'154d5.4','altitude':'37d17.4','assumedLat':'35d59.7','assumedLong':'74d35.3'}
-        expectedResult='0.581474846'
+        expectedResult=0.581474846
         self.assertAlmostEqual(DP.calcIntDist(values), expectedResult)
 
 ############sad path tests##############
