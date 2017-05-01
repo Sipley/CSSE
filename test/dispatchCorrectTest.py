b64dfcfe5a40baf1660518f10b0cec171c0927f4
Sipley <breanna.sipley@gmail.com>
2017-04-30 19:48:19 -0500

    Red Light |

--- test/dispatchCorrectTest.py
+++ test/dispatchCorrectTest.py
@@ -23,7 +23,7 @@ class MyTestCase(unittest.TestCase):
     def test100_040_shouldResultCorrectedDist(self):
         values={'op':'correct','lat':'89d20.1','long':'154d5.4','altitude':'37d17.4','assumedLat':'35d59.7','assumedLong':'74d35.3'}
         expectedResult=104
-        self.assertAlmostEqual(DP.calcCorrectedAlt(values), expectedResult)
+        self.assertAlmostEqual(DP.calcCorrectedAlt(values), expectedResult,0)
 
 ############sad path tests##############
 
