3d8f240004e4cdaf10098ad849a0f6914cd94f06
Sipley <breanna.sipley@gmail.com>
2017-04-30 20:49:24 -0500

    Red Light |

--- test/Sandbox.py
+++ test/Sandbox.py
@@ -1,53 +1,15 @@
 import math
 
-#values = {'op':'correct'}
+lat = 16.538
+assumedLat = -53.64
+long = 95.693
+assumedLong = 74.5883
+LHA = long + assumedLong
 
+A = math.sin(math.radians(lat))
+B = math.sin(math.radians(assumedLat))
+C = math.cos(math.radians(lat))
+D = math.cos(math.radians(assumedLat))
+E = math.cos(math.radians(LHA))
 
-#class Values(object):
-#    def __init__(self, op=None):
-#        self.op = op
-
-
-#result = Values(**values)
-#print result.op
-
-values={'op':'correct','lat':'89d20.1','long':'154d5.4','altitude':'37d17.4','assumedLat':'35d59.7','assumedLong':'74d35.3'}
-
-
-class Values(object):
-    def __init__(self, lat, long, altitude, assumedLat, assumedLong, op=None):
-        self.op = op
-        self.lat = convertAngleFromDeg(lat)
-        self.long = convertAngleFromDeg(long)
-        self.altitude = convertAngleFromDeg(altitude)
-        self.assumedLat = convertAngleFromDeg(assumedLat)
-        self.assumedLong = convertAngleFromDeg(assumedLong)
-
-#def checkDict(values):
-#    result = Values(**values).lat
-#    return result
-
-def convertAngleFromDeg(angle):
-    degree = angle.split('d')[0]
-    arcminute = float(angle.split('d')[1]) / 60
-    convertedAngle = float(degree) + arcminute
-    if '-' in degree:
-        convertedAngle = -convertedAngle
-    return convertedAngle
-
-def calcLHA(values):
-    LHA = Values(**values).long + Values(**values).assumedLong
-  #  LHAdeg = convertAngleToDeg(LHA)
-    return LHA
-
-def calcIntDist(values):
-    A = math.sin(math.radians(Values(**values).lat))
-    B = math.sin(math.radians(Values(**values).assumedLat))
-    C = math.cos(math.radians(Values(**values).lat))
-    D = math.cos(math.radians(Values(**values).assumedLat))
-    E = math.cos(calcLHA(values))
-    intDist = (A * B) + (C * D * E)
-    return intDist
-
-result = calcIntDist(values)
-print result
+print ((A * B) + (C * D * E))
--- test/dispatchCorrectTest.py
+++ test/dispatchCorrectTest.py
@@ -40,7 +40,7 @@ class MyTestCase(unittest.TestCase):
     #    expectedResult={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3', 'correctedDistance':'3940', 'correctedAzimuth':'164d42.9'}
     #    self.assertDictEqual(DP.dispatch(values),expectedResult)
 
-    def test100_080_shouldReturnIntDistance):
+    def test100_080_shouldReturnIntDistance(self):
         values={'op':'correct', 'lat': '16d32.3', 'long':'95d41.6', 'altitude':'13d42.3', 'assumedLat':'-53d38.4', 'assumedLong':'74d35.3'}
         expectedResult=-0.789410565
         self.assertAlmostEqual(DP.calcIntDist(values), expectedResult,2)
